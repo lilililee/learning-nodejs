98d24b2435af03d419729abf36aa95de434b1ebf
lilililee <liguang_hello@icloud.com>
2016-12-18 11:48:54 +0800

    create https server

---
+++ imooc/http/crawler2.js
@@ -0,0 +1,47 @@
+var http = require('http');
+//var cheerio = require('cheerio');
+var base_url = 'http://www.imooc.com/learn/259';
+
+// function catchSinglePage(id) {
+// 	return new Promise(function (resolve,reject) {
+// 		var url = base_url + id;
+// 		http.get(url,function(res){
+// 			var html = '';
+// 			//接收数据
+// 			res.on('data',function(chunk){
+// 				html += chunk;
+// 			});
+// 			//接受完后开始处理
+// 			res.on('end',function(){
+// 				console.log(html);
+// 				dealDate(html);
+// 				resolve();
+// 			});
+// 		});
+
+// 		function dealDate(html){
+
+// 		}
+// 	});
+// }
+
+//catchSinglePage(259);
+
+var url = base_url;
+http.get(url,function(res){
+	var html = '';
+	//接收数据
+	res.on('data',function(chunk){
+		html += chunk;
+	});
+	//接受完后开始处理
+	res.on('end',function(){
+		console.log(html);
+		dealDate(html);
+		//resolve();
+	});
+});
+
+function dealDate(html){
+
+}
